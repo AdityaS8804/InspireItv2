7dbc11c4800b0e2ba5d2437d7d3a54b2a6d5423c
AdityaS8804 <aditya.s2022a@vitstudent.ac.in>
2025-02-10 23:21:23 +0530

    Added basic endpoints with schemas

---
+++ Recommended/recommend.py
@@ -0,0 +1,7 @@
+def recommendAcceptButton(data):
+    sample_data = {
+        "title": "This is the title",
+        "Abstract": "This is the abstract",
+        "Methodology recommended": "This is the recommended methodology",
+    }
+    return sample_data
